75b9fd4000229588663787ec9b9f1aba2b3ed768
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2018-12-15 13:09:54 -0300

    sem situação

--- JS/productController.js
+++ JS/productController.js
@@ -2,7 +2,6 @@ function addProduto() {
 	let codigo = document.getElementById("codigo").value;
 	let nome = document.getElementById("nome").value;
 	let fabricante = document.getElementById("fabricante").value;
-	let situacao = document.getElementById("situacao").value;
 	let categoria = document.getElementById("categoria").value;
 	let preco = document.getElementById("preco").value;
 	let qtd = document.getElementById("quantidade").value;
@@ -12,7 +11,7 @@ function addProduto() {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({"codigo":codigo,"nome":nome,"fabricante":fabricante,"situacao":situacao,"categoria":categoria,"preco":preco,"quantidade":qtd,"desconto":"0"})
+    body: JSON.stringify({"codigo":codigo,"nome":nome,"fabricante":fabricante,"situacao":"true","categoria":categoria,"preco":preco,"quantidade":qtd,"desconto":"0"})
   });
 	window.alert("Produto adicionado!");
 }
